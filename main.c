7f71ff0063b2bfbc5facfc05f3961ff63c8cc1a8
Robert Ashby <ashby.r@northeastern.edu>
2024-03-27 16:30:05 -0400

    added framework for simple shell

--- main.c
+++ main.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+#define MAX_COMMAND_LENGTH 100
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        printf("$ ");
+        fflush(stdout);
+
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            break;
+        }
+
+        // Remove trailing newline character
+        command[strcspn(command, "\n")] = '\0';
+
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+            char *args[] = {command, NULL};
+            execvp(args[0], args);
+
+            // execvp only returns if an error occurs
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    return 0;
+}
