3637ad5918d67cd51d0816a0a4790b3285d14a6a
Robert Ashby <ashby.r@northeastern.edu>
2024-03-28 13:01:32 -0400

    pulled code used in HW, shell handles multiple inputs now

--- main.c
+++ main.c
@@ -1,44 +1,235 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 100
+#define MAX_SUB_COMMANDS 5
+#define MAX_ARGS 10
+
+// define subcommand data structure
+struct SubCommand {
+    char *line;
+    char *argv[MAX_ARGS];
+};
+
+// define command data structure
+struct Command {
+    struct SubCommand sub_commands[MAX_SUB_COMMANDS];
+    char *stdin_redirect;
+    char *stdout_redirect;
+    int background;
+    int num_sub_commands;
+};
+
+// function to read arguments from input string
+void ReadArgs(char *in, char **argv, int size) {
+
+    // declare token variable
+    char *token;
+
+    // tokenize input string using space as delimiter
+    token = strtok(in, " ");
+
+    // loop through tokens and add to argv array
+    int argc = 0;
+    while (token != NULL && argc < size) {
+
+        // add token to argv array
+        argv[argc] = strdup(token);
+        argc++;
+
+        // get next token
+        token = strtok(NULL, " ");
+    }
+
+    // null terminate argv array
+    argv[argc] = NULL;
+    
+
+}
+
+// function to print out arguments
+void PrintArgs(char **argv) {
+    int iter = 0;
+    // loop through argv array and print out by value until null value reached
+    while (argv[iter] != NULL) {
+        printf("argv[%d] = '%s'\n", iter, argv[iter]);
+        iter++;
+    }
+}
+
+void ReadRedirectsAndBackground(struct Command *command) {
+    // set stdin_redirect, stdout_redirect, and background fields of command to NULL and 0,
+    // values will be overwritten if they are found in the subcommands
+    command->stdin_redirect = NULL;
+    command->stdout_redirect = NULL;
+    command->background = 0;
+
+    // loop through subcommands in reverse order
+    for (int i = command->num_sub_commands - 1; i >= 0; i--) {
+
+        // declare iterator value
+        int argc = 0;
+
+        // loop through argument arrays until null value reached
+        while (command->sub_commands[i].argv[argc] != NULL) {
+
+            // check if argument is "<", set stdin_redirect to next argument and set current argument to NULL.
+            if (strcmp(command->sub_commands[i].argv[argc], "<") == 0) {
+                command->stdin_redirect = command->sub_commands[i].argv[argc + 1];
+                command->sub_commands[i].argv[argc] = NULL;
+
+            // check if argument is ">", set stdout_redirect to next argument and set current argument to NULL.
+            } else if (strcmp(command->sub_commands[i].argv[argc], ">") == 0) {
+                command->stdout_redirect = command->sub_commands[i].argv[argc + 1];
+                command->sub_commands[i].argv[argc] = NULL;
+
+            // check if argument is "&", set background to 1 and set current argument to NULL.
+            } else if (strcmp(command->sub_commands[i].argv[argc], "&") == 0) {
+                command->background = 1;
+                command->sub_commands[i].argv[argc] = NULL;
+            }
+
+            // increment argc
+            argc++;
+        }
+    }
+}
+
+// function to read command from input string
+void ReadCommand(char *line, struct Command *command) {
+    
+        // declare token variable
+        char *token;
+    
+        // tokenize input string using | as delimiter
+        token = strtok(line, "|");
+    
+        // loop through tokens and add to argv array
+        int argc = 0;
+        while (token != NULL && argc < MAX_SUB_COMMANDS) {
+    
+            // add token to argv array
+            command->sub_commands[argc].line = strdup(token);
+            argc++;
+    
+            // get next token
+            token = strtok(NULL, "|");
+        }
+    
+        // set number of sub commands
+        command->num_sub_commands = argc;
+
+        // call ReadArgs on each subcommand line
+        for (int i = 0; i < command->num_sub_commands; i++) {
+            ReadArgs(command->sub_commands[i].line, command->sub_commands[i].argv, MAX_ARGS);
+        }
+
+        // call ReadRedirectsAndBackground to set stdin_redirect, stdout_redirect, 
+        // and background fields of command
+        ReadRedirectsAndBackground(command);
+
+}
+
+// function to print out command data structure
+void PrintCommand(struct Command *command) {
+
+    // loop through subcommands and print out by value
+    for (int i = 0; i < command->num_sub_commands; i++) {
+        printf("Command %d:\n", i);
+        PrintArgs(command->sub_commands[i].argv);
+        printf("\n");
+    }
+
+    // print out stdin_redirect value
+    if (command->stdin_redirect != NULL) {
+        printf("Redirect stdin: %s\n", command->stdin_redirect);
+    } 
+    else {
+        printf("Redirect stdin: NULL\n");
+    }
+
+    // print out stdout_redirect value
+    if (command->stdout_redirect != NULL) {
+        printf("Redirect stdout: %s\n", command->stdout_redirect);
+    } 
+    else {
+        printf("Redirect stdout: NULL\n");
+    }
+
+    // print out background, yes or no based on value
+    if (command->background == 1) {
+        printf("Background: yes\n");
+    } 
+    else {
+        printf("Background: no\n");
+    }
+
+}
+
+// function to execute a command with its arguments
+void ExecuteCommand(struct Command *command) {
+    // create a child process
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        // error occurred while forking
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) { // child process
+
+        // execute the command
+        execvp(command->sub_commands[0].argv[0], command->sub_commands[0].argv);
+
+        // if execvp returns, an error occurred
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    } else { // parent process
+        
+        // wait for the child process to complete
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
+
+        // check if the child process exited normally
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+        }
+
+    }
+}
 
 int main() {
-    char command[MAX_COMMAND_LENGTH];
+    
+    // declare variables
+    char line[100];
+    struct Command command;
 
+    // loop until user enters "exit"
     while (1) {
+        // prompt user for input
         printf("$ ");
-        fflush(stdout);
 
-        if (fgets(command, sizeof(command), stdin) == NULL) {
+        // read input from user
+        fgets(line, sizeof(line), stdin);
+
+        // remove newline character from input
+        line[strcspn(line, "\n")] = '\0';
+
+        // check if user entered "exit"
+        if (strcmp(line, "exit") == 0) {
             break;
         }
 
-        // Remove trailing newline character
-        command[strcspn(command, "\n")] = '\0';
+        // read command from input
+        ReadCommand(line, &command);
 
-        pid_t pid = fork();
-
-        if (pid < 0) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            // Child process
-            char *args[] = {command, NULL};
-            execvp(args[0], args);
-
-            // execvp only returns if an error occurs
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        } else {
-            // Parent process
-            int status;
-            waitpid(pid, &status, 0);
-        }
+        // execute command
+        ExecuteCommand(&command);
     }
 
-    return 0;
+        return 0;
 }
